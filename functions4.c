fa9552ad66d5cdd8b96ec681a742b2e2c4f8d6ab
AnasBenba <anasbenba123@gmail.com>
2023-05-25 09:05:25 -0700

    fix code

--- functions4.c
+++ functions4.c
@@ -12,13 +12,10 @@ void pchar(stack_t **stack, unsigned int line_number)
 		fprintf(stderr ,"L%i: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n >= 0 || (*stack)->n <= 127)
+	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
-		printf("%c\n", (*stack)->n);
-	}
-	else
-	{
-		fprintf(stderr ,"L%i: can't pchar, value out of range", line_number);
+		fprintf(stderr ,"L%i: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	printf("%c\n", (*stack)->n);
 }
